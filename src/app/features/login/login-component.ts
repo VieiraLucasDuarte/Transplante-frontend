cf4149ae50aebdd6f2493f14f603467a0cf3ca4e
Lucas Duarte Vieira <lucas.vieira@fulltechpro.com.br>
2025-06-03 15:52:36 -0300

    Criado novo componente e nova rota

---
+++ src/app/features/login/login-component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-component',
+  imports: [],
+  templateUrl: './login-component.html',
+  styleUrl: './login-component.scss'
+})
+export class LoginComponent implements OnInit {
+  ngOnInit(): void {
+  }
+
+  login() {
+  }
+
+}
